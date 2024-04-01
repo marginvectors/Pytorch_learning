ddc52e6a5927c66cb12d205e88d42213faab8362
Mary <741536126@qq.com>
2024-04-01 16:29:50 +0800

    README.md

--- main.py
+++ main.py
@@ -18,5 +18,21 @@ class Adam(GenericAdaptiveOptimizer):
     def init_state(self,state:Dict[str,any],group: Dict[str,any],param:nn.Parameter):
         state['step'] = 0
         state['exp_avg'] = torch.zeros_like(param,memory_format=torch.preserve_format)
-        state['exp_avg_sq'] = torch.zeros_like(param,memory_format=torch.preserve_format)
+        state['exp_avg_sq'] = torch.zeros_like(param,memory_format=torch.preserve_format) # 
+    
+    def get_mv(self,state:Dict[str,Any],grad:torch.Tensor):
+        beta1,beta2 = group('betas')
+        m,v = state['exp_avg'],state['exp_avg_sq'] #获取m_{t-1},v_{t-1}
+        m.mul_(beta1).add_(grad,alpha=1-beta1) #更新m_t, 
+        v.mul_(beta2).addcmul_(grad,grad,value=1-beta2)
+        return m,v
+    
+    def get_lr(self,state:Dict[str,any],group:Dict[str,any]):
+        return group['lr']
+    
+    def adam_update(self,state:Dict[str,any],group:Dict[str,any],param:torch.nn.Parameter,m:torch.Tensor,v:torch.Tensor):
+
+
+
+
         
